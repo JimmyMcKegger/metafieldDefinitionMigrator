9044714426d59ee108f40e4ddb2562193d6b7e9c
Markus Voigt <markus.voigt90@gmail.com>
2023-03-10 16:20:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Shopify Metafield Migrator
+
+A Node.js script that migrates Shopify Metafield definitions from a source store to a destination store using the Shopify Admin API.
+
+## Getting Started
+
+1.  Clone this repository to your local machine.
+
+2.  Install the dependencies by running `npm install` in the project directory.
+
+3.  [Create a custom app inside the Shopify Admin Dashboard](https://help.shopify.com/en/manual/apps/app-types/custom-apps) for the source and destination store. Permissions required: write_metaobject_definitions, read_metaobject_definitions all well as write access to all owner types you want to migrate (customers, orders, content, ...)
+
+4.  Replace the STORE, SHOPIFY_TOKEN and SHOPIY_API_VER constants for the source and destination store inside the migrate.js file.
+
+5.  Run the script using the command `npm start`.
+
+## Usage
+
+The script will migrate the metafield definitions defined in the `OWNER_TYPES` array from the source store to the destination store. It will skip existing metafield definitions.
+
+## Limitations
+
+- The script only migrates metafield _definitions_. It does not migrate the _values_ of the metafields.
+- The script does not handle errors caused by invalid credentials, network issues, or other issues with the Shopify API.
---
+++ migrate.js
@@ -0,0 +1,152 @@
+import axios from "axios";
+
+// Setup the type of resources for which you want to migrate the metafield defintions. Defaults to all.
+// More information: https://shopify.dev/docs/api/admin-graphql/2023-01/enums/MetafieldOwnerType
+
+const OWNER_TYPES = [
+  "ARTICLE",
+  "BLOG",
+  "COLLECTION",
+  "CUSTOMER",
+  "COMPANY",
+  "COMPANY_LOCATION",
+  "DRAFTORDER",
+  "LOCATION",
+  "ORDER",
+  "PAGE",
+  "PRODUCT",
+  "PRODUCTIMAGE",
+  "PRODUCTVARIANT",
+  "SHOP",
+];
+
+// Setup a custom app inside the Shopify Admin Dashboard for the source and destination store. Grant the app the permission to read and write metafield/metaobject definitions and grant write access to all affected resources.
+// More information: https://help.shopify.com/en/manual/apps/app-types/custom-apps
+
+const SOURCE = {
+  STORE: "source.myshopify.com",
+  SHOPIFY_TOKEN: "",
+  SHOPIFY_API_VER: "2023-01",
+};
+
+const DESTINATION = {
+  STORE: "destination.myshopify.com",
+  SHOPIFY_TOKEN: "",
+  SHOPIFY_API_VER: "2023-01",
+};
+
+const querySource = async (query, variables) => {
+  const response = await axios({
+    url: `https://${SOURCE.STORE}/admin/api/${SOURCE.SHOPIFY_API_VER}/graphql.json`,
+    method: "POST",
+    headers: {
+      "X-Shopify-Access-Token": SOURCE.SHOPIFY_TOKEN,
+      Accept: "application/json",
+    },
+    data: {
+      query,
+      variables,
+    },
+  });
+  if (response.error) {
+    console.error(response.error);
+    throw new Error("Error accessing graphql");
+  }
+
+  return response.data.data;
+};
+
+const queryDestination = async (query, variables) => {
+  const response = await axios({
+    url: `https://${DESTINATION.STORE}/admin/api/${DESTINATION.SHOPIFY_API_VER}/graphql.json`,
+    method: "POST",
+    headers: {
+      "X-Shopify-Access-Token": DESTINATION.SHOPIFY_TOKEN,
+      Accept: "application/json",
+    },
+    data: {
+      query,
+      variables,
+    },
+  });
+  if (response.error) {
+    console.error(response.error);
+    throw new Error("Error accessing graphql");
+  }
+  return response.data.data;
+};
+
+OWNER_TYPES.forEach(async (OWNER_TYPE) => {
+  const queryResponse = await querySource(`
+  {
+    metafieldDefinitions(first: 20, ownerType: ${OWNER_TYPE}) {
+      edges {
+        node {
+          name
+          description
+          key
+          namespace
+          ownerType
+          type{
+            name
+          }
+          validations {
+            name
+            type
+            value
+          }
+        }
+      }
+    }
+  }
+`);
+  if (!queryResponse?.metafieldDefinitions?.edges?.length > 0) {
+    console.log(
+      `Error fetching metafield definitions for owner type: ${OWNER_TYPE}`
+    );
+    return;
+  }
+  const metafieldDefinitions = queryResponse.metafieldDefinitions.edges.map(
+    (node) => node.node
+  );
+
+  metafieldDefinitions.forEach(async (definition) => {
+    const creationResponse = await queryDestination(
+      `
+  mutation metafieldDefinitionCreate($definition: MetafieldDefinitionInput!) {
+    metafieldDefinitionCreate(definition: $definition) {
+      createdDefinition {
+        id
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`,
+      {
+        definition: {
+          description: definition.description,
+          key: definition.key,
+          name: definition.name,
+          namespace: definition.namespace,
+          ownerType: definition.ownerType,
+          pin: definition.pin,
+          type: definition.type.name,
+          validations: definition.validations.map((validation) => {
+            return {
+              name: validation.name,
+              value: validation.value,
+            };
+          }),
+          visibleToStorefrontApi: definition.visibleToStorefrontApi,
+        },
+      }
+    );
+    if (creationResponse.metafieldDefinitionCreate?.userErrors?.length > 0)
+      console.log(
+        `Failed to copy metafield definition with key ${definition.key} because ${creationResponse.metafieldDefinitionCreate.userErrors[0].message}`
+      );
+  });
+});
